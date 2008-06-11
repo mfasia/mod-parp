93250507ce001c7530d2643fc87b0f8be0280215
ia97lies <ia97lies>
2008-06-11 05:34:59 +0000

    Bugfix

--- patch.sh
+++ patch.sh
@@ -9,12 +9,12 @@ mkdir $MODULE
 cd $MODULE
 
 for E in `find ../../../httpd_src/modules/$MODULE -type f | grep -v CVS `; do
-  rm `basename $E`
+  rm -f `basename $E`
   ln -s $E `basename $E`
 done
 
 cd ../../support
 for E in `find ../../httpd_src/support -type f | grep -v CVS `; do
-  rm `basename $E`
+  rm -f `basename $E`
   ln -s $E `basename $E`
 done
