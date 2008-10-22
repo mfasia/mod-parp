3b77bd157cd2e48eab0361ad8650554646c77083
pbuchbinder <pbuchbinder>
2008-10-22 18:44:59 +0000

    *** empty log message ***

--- test/test.sh
+++ test/test.sh
@@ -6,13 +6,28 @@ ERRORS=0
 WARNINGS=0
 
 ./ctl.sh start
+ps -Ao vsz,comm,pid,ppid | grep `cat Server/logs/pid` | sort -n | tail -1
 
 ./htt.sh -s scripts/main_func.htt
 if [ $? -ne 0 ]; then
     ERRORS=`expr $ERRORS + 1`
     echo "FAILED main_func.htt"
 fi
+./htt.sh -s scripts/loop.htt
+if [ $? -ne 0 ]; then
+    ERRORS=`expr $ERRORS + 1`
+    echo "FAILED loop.htt"
+fi
+for E in `seq 7`; do
+ps -Ao vsz,comm,pid,ppid | grep `cat Server/logs/pid` | sort -n | tail -1
+./htt.sh -s scripts/big.htt
+if [ $? -ne 0 ]; then
+    ERRORS=`expr $ERRORS + 1`
+    echo "FAILED big.htt"
+fi
+done
 
+ps -Ao vsz,comm,pid,ppid | grep `cat Server/logs/pid` | sort -n | tail -1
 ./ctl.sh stop
 
 if [ $WARNINGS -ne 0 ]; then
