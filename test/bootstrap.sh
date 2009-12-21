1c6dcb4f1d414c7525531fa4453f2cb737ccda3f
pbuchbinder <pbuchbinder>
2009-12-21 18:53:37 +0000

    *** empty log message ***

--- test/bootstrap.sh
+++ test/bootstrap.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 SERVER_ROOT=`pwd`/Server
 
+QS_UID=`id`
+QS_UID_STR=`expr "$QS_UID" : 'uid=[0-9]*.\([a-z,A-Z,0-9,_]*\)'`
+
 sed < Server/conf/httpd.conf.tmpl > Server/conf/httpd.conf \
-  -e "s;##SERVER_ROOT##;$SERVER_ROOT;g"
+  -e "s;##SERVER_ROOT##;$SERVER_ROOT;g" \
+  -e "s;##USER##;$QS_UID_STR;g"
 
