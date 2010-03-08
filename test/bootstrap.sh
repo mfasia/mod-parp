2623160de7e1de2905f036396a0c4839628a45ab
pbuchbinder <pbuchbinder>
2010-03-08 15:07:15 +0000

    *** empty log message ***

--- test/bootstrap.sh
+++ test/bootstrap.sh
@@ -8,3 +8,13 @@ sed < Server/conf/httpd.conf.tmpl > Server/conf/httpd.conf \
   -e "s;##SERVER_ROOT##;$SERVER_ROOT;g" \
   -e "s;##USER##;$QS_UID_STR;g"
 
+COMPS="logs conf"
+for E in $COMPS; do
+    if [ ! -r $E ]; then
+	ln -s `pwd`/Server/$E .
+    fi
+done
+
+if [ ! -x run.sh ]; then
+    ln -s `pwd`/htt.sh run.sh
+fi
